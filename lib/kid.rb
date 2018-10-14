f5840015322a7d55ceddafa673ccebea74109a4a
Peter Kreppein <pmkreppein@gmail.com>
2018-10-14 23:36:51 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,7 +3,7 @@ require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
 class Kid
- 
+ attr_accessor :name
 extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
  
